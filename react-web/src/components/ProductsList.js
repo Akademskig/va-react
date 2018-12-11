f016663a355ea3087e8ba500bab7029707b5e50e
Marta Sušek <marta.susek@uniqcast.com>
2018-12-11 17:12:59 +0100

    Add Product component and images

--- react-web/src/components/ProductsList.js
+++ react-web/src/components/ProductsList.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react"
 import { Button } from 'semantic-ui-react'
+import Product from './Product'
 
 class ProductsList extends Component {
     render() {
         return (
             <div>
                 <div className='ui unstackable items'>
-                    Hello, friend! I am a basic React component.
-            </div><Button>And I'm a button</Button>
+                    <Product />
+                </div><Button>Vote!</Button>
             </div>
         )
     }
